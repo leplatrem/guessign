b6584ae8fed49fae6f818c701cde76a8d9ca6dd8
Mathieu Leplatre <contact@mathieu-leplatre.info>
2014-12-06 00:53:42 +0100

    Clean-up

--- main.js
+++ main.js
@@ -39,11 +39,13 @@ var GameControls = React.createClass({
 
   onChange: function (event) {
     this.config[event.target.name] = event.target.value;
+    // Remove empty values
+    for(var k in this.config)
+      if(!this.config[k]) delete this.config[k];
     this.props.onConfigure(this.config);
   },
 
   render: function() {
-
     var comboBox = function (property, options) {
       return <div className="col {property}">
         <select name={property} onChange={this.onChange}>
@@ -56,12 +58,21 @@ var GameControls = React.createClass({
     }
 
     return <div className="row controls">
-      <div className="col">Guessing signs</div>
+      <div class="col">Settings</div>
       {comboBox.call(this, 'lang', this.props.langs)}
       {comboBox.call(this, 'difficulty', this.props.difficulties)}
       {comboBox.call(this, 'category', this.props.categories)}
-      <div className="score col">{this.props.score}/{this.props.total}</div>
   </div>;
+  }
+});
+
+
+var Scores = React.createClass({
+  render: function () {
+    return <div className="score col">
+      <span className="good">{this.props.score}</span>
+      <span className="bad">{this.props.total - this.props.score}</span>
+    </div>
   }
 });
 
@@ -77,64 +88,54 @@ var GameApp = React.createClass({
     };
   },
 
-  getFilters: function () {
-    var filters = {
-      lang: this.state.lang,
-      difficulty: this.state.difficulty,
-      category: this.state.category
-    };
-    for(var k in filters)
-      if(!filters[k]) delete filters[k];
-    return filters;
-  },
-
   onConfigure: function (config) {
-    var newstate = _.extend(this.getInitialState(), config);
+    var matching = _.where(this.props.database, config);
+    var newstate = _.extend(this.getInitialState(), {levels: matching});
     this.setState(newstate);
-    var matching = _.where(this.props.database, this.getFilters());
-    this.setState({levels: matching});
   },
 
   facetList: function (property) {
-    var filters = this.getFilters();
-    delete filters[property];
-    var matching = _.where(this.props.database, filters);
-    var facets = _.pluck(matching, property);
+    var facets = _.pluck(this.state.levels, property);
     return _.uniq(facets.sort(), true);
   },
 
   onPlay: function (success) {
-    var score = this.state.score;
-    var next = this.state.current;
-
-    // Jump to next level if correct
     if (success) {
-      score++;
-      next = ++next % this.state.levels.length;
+      var next = (this.state.current + 1) % this.state.levels.length;
+      this.setState({
+        score: this.state.score + 1,
+        current: next
+      });
     }
-
-    this.setState({
-      current: next,
-      score: score,
-      total: this.state.total + 1
-    });
+    this.setState({total: this.state.total + 1});
   },
 
   render: function() {
     var level = this.state.levels[this.state.current];
+
     return <div>
-      <GameControls onConfigure={this.onConfigure}
-                    langs={this.facetList('lang')}
-                    difficulties={this.facetList('difficulty')}
-                    categories={this.facetList('category')}
-                    score={this.state.score}
-                    total={this.state.total} />
+      <div className="infos row">
+        <span className="title col">Guessing signs</span>
+        <span className="lang col">{level.lang}</span>
+        <span className="difficulty col">{level.difficulty}</span>
+        <span className="category col">{level.category}</span>
+        <div className="col">
+          <Scores score={this.state.score}
+                  total={this.state.total} />
+        </div>
+      </div>
+
       <div className="content row">
         <VideoPlayer video={level.video} />
         <WordsCloud word={level.word}
                     words={level.words}
                     onPlay={this.onPlay} />
       </div>
+
+      <GameControls onConfigure={this.onConfigure}
+                    langs={this.facetList('lang')}
+                    difficulties={this.facetList('difficulty')}
+                    categories={this.facetList('category')} />
     </div>;
   }
 });
