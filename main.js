31c912e7d04b29ea34eeb57ecba6701d97a0f6d5
Mathieu Leplatre <contact@mathieu-leplatre.info>
2014-12-05 22:35:48 +0100

    Jump levels

--- main.js
+++ main.js
@@ -23,27 +23,19 @@ var VideoPlayer = React.createClass({
 
 
 var WordsCloud = React.createClass({
-  render: function() {
-    var onPlay = this.props.onPlay;
-    var wordButton = function(word) {
-      return <div className="button" onClick={onPlay.bind(null, word)}>{word}</div>;
-    };
-    return <div className="words col">{this.props.words.map(wordButton)}</div>;
-  }
-});
-
-
-var GameLevel = React.createClass({
-  onPlay: function (word) {
-    if (word == this.props.level.word) {
-      alert('Yeah!');
-    }
+  onWordClick: function (word) {
+    var success = word == this.props.word;
+    this.props.onPlay(success);
   },
 
   render: function() {
-    return <div className="content row">
-      <VideoPlayer video={this.props.level.video}/>
-      <WordsCloud words={this.props.level.words} onPlay={this.onPlay}/>
+    var wordButton = function(word) {
+      var onWordClick = this.onWordClick.bind(this, word);
+      return <div className="button"
+                  onClick={onWordClick}>{word}</div>;
+    };
+    return <div className="words col">
+      {this.props.words.map(wordButton.bind(this))}
     </div>;
   }
 });
@@ -52,32 +44,73 @@ var GameLevel = React.createClass({
 var GameControls = React.createClass({
   render: function() {
     return <div className="row controls">
-      <div className="col">Guess signs</div>
+      <div className="col">Guessing signs</div>
       <div className="col lang">
         <select name="lang">
-            <option>LSF</option>
+          <option value="fr">LSF (Fr)</option>
+          <option value="en-US">ASL (USA)</option>
         </select>
       </div>
       <div className="col difficulty">
-        <select name="lang">
-            <option>Easy</option>
+        <select name="difficulty">
+          <option>Easy</option>
+          <option>Medium</option>
+          <option>Hard</option>
         </select>
       </div>
-      <div className="score col">10/20</div>
+      <div className="col category">
+        <select name="category">
+          <option>Verb</option>
+          <option>Colour</option>
+          <option>Music</option>
+        </select>
+      </div>
+      <div className="score col">{this.props.score}/{this.props.total}</div>
   </div>;
   }
 });
 
 
 var GameApp = React.createClass({
+  getInitialState: function() {
+    return {
+      current: 0,
+      score: 0,
+      total: 0
+    };
+  },
+
+  onPlay: function (success) {
+    var score = this.state.score;
+    var next = this.state.current;
+
+    // Jump to next level if correct
+    if (success) {
+      score++;
+      next = ++next % this.props.database.length;
+    }
+
+    this.setState({
+      current: next,
+      score: score,
+      total: this.state.total + 1
+    });
+  },
+
   render: function() {
-    var currentLevel = levels[0];
+    var level = this.props.database[this.state.current];
     return <div>
-      <GameControls />
-      <GameLevel level={currentLevel}/>
+      <GameControls score={this.state.score}
+                    total={this.state.total} />
+      <div className="content row">
+        <VideoPlayer video={level.video} />
+        <WordsCloud word={level.word}
+                    words={level.words}
+                    onPlay={this.onPlay} />
+      </div>
     </div>;
   }
 });
 
 
-React.renderComponent(<GameApp />, document.getElementById('container'));
+React.renderComponent(<GameApp database={levels} />, document.getElementById('container'));
